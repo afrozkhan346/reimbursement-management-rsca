8972addbce6b0159f7f0f660e9337d6fdde44e80
ChaitraliTikar <chaitrali678@gmail.com>
2026-03-29 15:15:32 +0530

    feat: added admin user creation with role protection and expense module created

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const sequelize = require("./config/db");
 
+require("./models/Company");
+require("./models/user");
+require("./models/ExpenseCategory");
+require("./models/Expense");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/authRoutes"));
+app.use("/admin", require("./routes/adminRoutes"));
+app.use("/expenses", require("./routes/expenseRoutes"));
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(process.env.PORT || 5000, () => {
